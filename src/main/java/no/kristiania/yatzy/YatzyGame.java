de779e4d3c88850596e797dbd61d8a6c359a348a
Odin <odin.berg@hotmail.co.uk>
2020-08-26 17:21:19 +0200

    One,Two,ThreeOfAKind green. Pair, red

--- src/main/java/no/kristiania/yatzy/YatzyGame.java
+++ src/main/java/no/kristiania/yatzy/YatzyGame.java
@@ -3,34 +3,33 @@ package no.kristiania.yatzy;
 public class YatzyGame {
     public int score(YatzyCategory category, int[] dice) {
         int result = 0;
+        int[] frequencies = new int[7];
         for (int die : dice) {
-            if (die == 1 && category == YatzyCategory.ONES) {
-                result += die;
-            } else if (die == 2 && category == YatzyCategory.TWOS) {
-                result += die;
-            }
-            if (category == YatzyCategory.PAIR) {
-                int[] frequencies = new int[7];
-                for (int die : dice) {
-                    frequencies[die]++;
+            frequencies[die]++;
+        }
+        if (category == YatzyCategory.PAIR) {
+            for (int value = frequencies.length - 1; value >= 0; value--) {
+                if (frequencies[value] == 2) {
+                    return value * 2;
                 }
-
-                for (int value = 0; value < frequencies.length; value++) {
-                    if(frequencies[value] == 2) {
-                        return value*2;
-                    }
-                }
-
-                for (int i = 0; i < dice.length; i++) {
-                    for (int j = 0; j < dice.length; j++) {
-                        if (i != j && dice[i] == dice[j]){
-                            return dice[i]*2;
-                        }
-                    }
-                }
-                return 10;
             }
         }
-        return result;
+        if (category == YatzyCategory.THREE_OF_A_KIND) {
+            for (int value = frequencies.length - 1; value >= 0; value--) {
+                if (frequencies[value] == 3) {
+                    return value * 3;
+                }
+            }
+        }
+        if (category == YatzyCategory.ONES) {
+            return frequencies[1];
+            }
+        if (category == YatzyCategory.TWOS) {
+                return frequencies[2] * 2;
+            }
+            return result;
+        }
     }
-}
+
+
+
